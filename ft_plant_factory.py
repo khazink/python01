52e323e1ef48cc56739a149674054b3e71efee52
khazink <khairellkhazin@gmail.com>
2026-03-01 02:01:42 +0800

    plant factory

---
+++ ft_plant_factory.py
@@ -0,0 +1,17 @@
+class Plant:
+    def __init__(self, name, start_height, start_age):
+        self.name = name
+        self.height = start_height
+        self.age = start_age
+
+def main():
+    names = ["Rose", "Oak", "Cactus", "Sunflower", "Fern"]
+    heights = [25, 200, 5, 80, 15]
+    ages = [30, 365, 90, 45, 120]
+    my_plant = []
+    for i in range(names.len())
+        cur_name = names[i]
+        cur_height = heights[i]
+        cur_age = ages[i]
+        my_plants.append(new_plant)
+        print(f"Created: {cur_name} ({cur_height}, {cur_age} days)")
