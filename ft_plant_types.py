1515fd596099f1aa2c55f63902d0bbd80e34a962
Kaman <khairell-khazin-bin.kaman@hp.com>
2026-03-02 15:50:04 +0800

    buat sikit

--- ft_plant_types.py
+++ ft_plant_types.py
@@ -9,7 +9,7 @@ class Flower(Plant):
     def __init__(self, name, height, age, color):
         super().__init__(name, height, age)
         self.color = color
-
+        print(
     def bloom():
         print(f"{self.name} is blooming beautifully!")
 
@@ -18,17 +18,29 @@ class Tree(Plant):
     def __init__(self, name, height, age, trunk_diameter):
        super().__init__(name, height, age)
        self.trunk_dia = trunk_diameter
-
+        
     def produce_shade():
         print(f"{self.name} provide {self.trunk_dia * self.trunk_dia * 3.14}"
               " square meters of shade")
 
 
 class Vegetable(Plant):
-    def __init__(self, name, height, age, harvest_season):
+    def __init__(self, name, height, age, harvest_season, nutritional_value):
         super().__init__(name, height, age)
         self.harvest_season = harvest_season
+        print(f"{self.name} (Vegetable): {self.height}cm, {self.age} days, "
+             "{harvest_season}"
+        print(f"{self.name} is rich in {nutritional_value")
 
-    def nutritional_value()
-        print(f"{self.name} is rich in vitamin C)
-        
+
+def main():
+    print("=== Garden Plant Types ===")
+    rose = Flower("Rose", 25, 30, "red")
+    rose.bloom()
+    oak = Tree("Oak", 500, 1825, 50)
+    oak.produce_shade()
+    tomato = Vegetable("tomato", 80, 90, "summer", "vitamin C")
+
+if __name__ == "__main__":
+    main()
+
